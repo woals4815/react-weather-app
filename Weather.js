4785aa2adb095de97723188daaf0cef0691136d8
woals4815 <71315300+woals4815@users.noreply.github.com>
2020-11-10 22:59:40 +0900

    make icons and halfContainer

--- Weather.js
+++ Weather.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import PropTypes from "prop-types";
+import { MaterialCommunityIcons} from "@expo/vector-icons";
 
 
 export default function Weather({temp}) {
     return (
         <View style={styles.container}>
-            <Text>{temp}</Text>
+            <View style={styles.halfContainer}>
+                <MaterialCommunityIcons size={96} name="weather-lightning-rainy" />
+                <Text style={styles.temp}>{temp}</Text>
+            </View>
+            <View style={styles.halfContainer}></View>
         </View>
     );
 }
@@ -29,6 +34,14 @@ Weather.propTypes = {
 
 const styles = StyleSheet.create({
     container: {
+        flex:1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    temp: {
+        fontSize: 42
+    },
+    halfContainer: {
         flex:1,
         justifyContent: "center",
         alignItems: "center"
