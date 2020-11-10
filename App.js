c3c9a8238610aba45e21177be2a60df54bb2f70b
woals4815 <71315300+woals4815@users.noreply.github.com>
2020-11-10 20:22:59 +0900

    make temp prop and condition prop

--- App.js
+++ App.js
@@ -2,10 +2,25 @@ import React from 'react';
 import Loading from './Loading';
 import * as Location from "expo-location";
 import { Alert } from 'react-native';
+import axios from 'axios';
+import Weather from './Weather';
+
+const API_KEY = "6e3dfd30a51e72a5f611a01e66d9d318";
 
 export default class extends React.Component {
   state = {
     isLoading: true
+  };
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: {temp},
+        weather
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    this.setState({ isLoading: false, condition: weather[0].main, temp});
   };
   getLocation = async () => {
     try{
@@ -13,6 +28,7 @@ export default class extends React.Component {
       const {
         coords: { latitude, longitude}
       } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch(error){
       Alert.alert("Can't find you.", "Fuck off");
@@ -22,7 +38,7 @@ export default class extends React.Component {
     this.getLocation();
   }
   render(){
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={temp} condition={condition} />;
   }
 }
